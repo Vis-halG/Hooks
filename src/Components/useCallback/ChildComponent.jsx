514103e3158c387a6b0cb8b9e275db51e3a7f602
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2026-01-16 07:06:32 +0530

    update

--- src/Components/useCallback/ChildComponent.jsx
+++ src/Components/useCallback/ChildComponent.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-function ChildComponent(props) {
+const ChildComponent =React.memo(
+    (props)=> {
     console.log("udfkszubfgvdksi")
   return (
     <div>
       <button>{props.name}</button>
     </div>
   );
-}
+})
 
 export default ChildComponent;
+
+
+
+
+
+
+// import React from "react";
+
+// function ChildComponent(props) {
+//   console.log("udfkszubfgvdksi");
+//   return (
+//     <div>
+//       <button>{props.name}</button>
+//     </div>
+//   );
+// }
+
+// export default React.memo(ChildComponent);
